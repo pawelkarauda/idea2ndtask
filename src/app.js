705ddd17f4d8988d7a2de352b5022a33ab4c8998
Pawel <zlapmniehakerze@gmail.com>
2018-06-08 19:10:32 +0200

    Initial commit.

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+import Layout from './componenets/Layout';
+
+
+ReactDOM.render(<Layout />,  document.getElementById('app'));
+
