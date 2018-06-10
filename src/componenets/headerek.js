1783f4d7cbd0df22e413efe4dcf96aa49c61cc15
Pawel <zlapmniehakerze@gmail.com>
2018-06-10 19:14:28 +0200

    Project done

--- src/componenets/headerek.js
+++ src/componenets/headerek.js
@@ -1,24 +1,9 @@
 import React from "react";
 import Logo from "../images/logo_im_dark.png";
 import FlightInfo from "./FlightInfo";
+import ResponsiveMenu from "react-responsive-navbar";
 
 class Header extends React.Component {
-    burgerToggle() {
-        let menu = document.querySelector(".Header__nav__mobile");
-        let hamburger = document.querySelector(".Header__nav__item--icon");
-        if (hamburger.innerText === "Menu") {
-            hamburger.innerText = "Zamknij";
-        } else {
-            hamburger.innerText = "Menu";
-        }
-
-        if (menu.style.display === "flex") {
-            menu.style.display = "none";
-        } else {
-            menu.style.display = "flex";
-        }
-    }
-
     render() {
         return (
             <header className="Header">
@@ -28,6 +13,11 @@ class Header extends React.Component {
                     </div>
 
                     <nav className="Header__nav">
+                        <div className="Header__hamburger">
+                            <a className="Header__nav__item--icon" href="#">
+                                Menu
+                            </a>
+                        </div>
                         <ul className="Header__nav__list">
                             <li className="Header__nav__item">
                                 <a className="Header__nav__item--link" href="#">
@@ -78,67 +68,6 @@ class Header extends React.Component {
                                 </a>
                             </li>
                         </ul>
-
-                        <ul className="Header__nav__mobile">
-                            <li className="Header__nav__item">
-                                <a className="Header__nav__item--link" href="#">
-                                    About us
-                                </a>
-                            </li>
-                            <li className="Header__nav__item">
-                                <a className="Header__nav__item--link" href="#">
-                                    Services
-                                </a>
-                            </li>
-                            <li className="Header__nav__item">
-                                <a className="Header__nav__item--link" href="#">
-                                    Clients
-                                </a>
-                            </li>
-                            <li className="Header__nav__item">
-                                <a className="Header__nav__item--link" href="#">
-                                    Case studies
-                                </a>
-                            </li>
-                            <li className="Header__nav__item">
-                                <a className="Header__nav__item--link" href="#">
-                                    Process
-                                </a>
-                            </li>
-                            <li className="Header__nav__item">
-                                <a className="Header__nav__item--link" href="#">
-                                    Careers
-                                </a>
-                            </li>
-                            <li className="Header__nav__item">
-                                <a className="Header__nav__item--link" href="#">
-                                    Blog
-                                </a>
-                            </li>
-                            <li className="Header__nav__item">
-                                <a className="Header__nav__item--link" href="#">
-                                    Contact
-                                </a>
-                            </li>
-                            <li className="Header__nav__item">
-                                <a
-                                    className="Header__nav__item--link Header__nav__item--link--cat"
-                                    href="#"
-                                >
-                                    Estimate project
-                                </a>
-                            </li>
-                        </ul>
-
-                        <div className="Header__hamburger">
-                            <a
-                                onClick={this.burgerToggle}
-                                className="Header__nav__item--icon"
-                                href="#"
-                            >
-                                Menu
-                            </a>
-                        </div>
                     </nav>
                 </div>
                 <div className="container">
